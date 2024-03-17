0e222895c488a85d82fcdfbb1a35f1d96c342667
invictuz <dan1@hotmail.es>
2024-03-17 18:19:01 +0100

    nivel 3 completo

--- src/main/java/TEMA_1/NIVEL_3/Commands/AccelerateCommand.java
+++ src/main/java/TEMA_1/NIVEL_3/Commands/AccelerateCommand.java
@@ -7,6 +7,8 @@ public class AccelerateCommand implements Command {
 
     private Vehicle vehicle;
 
+
+
     public AccelerateCommand(Vehicle vehicle) {
         this.vehicle = vehicle;
     }
--- src/main/java/TEMA_1/NIVEL_3/Interfaces/Command.java
+++ src/main/java/TEMA_1/NIVEL_3/Interfaces/Command.java
@@ -2,4 +2,5 @@ package TEMA_1.NIVEL_3.Interfaces;
 
 public interface Command {
     void execute();
+
 }
